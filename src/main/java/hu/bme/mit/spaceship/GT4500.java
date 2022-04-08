f86a7e459afb97b21bbc1b92b0b95013d912adfd
hangyalpeter <hangyal.peter@gmail.com>
2022-04-08 09:18:21 +0200

    Change comment

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
+        // try to fire both of the torpedo stores changing this line in another way
 
         boolean primarySuccess = false;
         boolean secondarySuccess = false;
